61d070a45ea7bd3ded4a2cdf400a5bbe6da754b8
Dongyu Tian <2502533535@qq.com>
2025-04-14 14:05:54 +0800

    fix: fix bug of noise speed scatter in t-s figure

--- time_space_plot.py
+++ time_space_plot.py
@@ -18,7 +18,7 @@ class TimeSpacePlotter(ResearchPlt):
     def __init__(
             self,
             path: str,
-            output_dir: str,
+            save_dir: str,
             lane_idx: Union[int, str],
             car_idx: Union[int, str],
             time_idx: Union[int, str],
@@ -27,6 +27,7 @@ class TimeSpacePlotter(ResearchPlt):
             max_time: int = 1e10,
             v_trans: bool = False,
             cmap = cm.jet_r,
+            ids: Union[list, str, int] = None,
             markersize: int = 1,
             **kwargs,
             ):
@@ -35,7 +36,7 @@ class TimeSpacePlotter(ResearchPlt):
         input
         -----
         path: str, 仿真数据csv文件路径
-        output_dir: str, 保存图片的文件夹
+        save_dir: str, 保存图片的文件夹
         lane_idx: Union[int, str], 车道号列索引或列名
         car_idx: Union[int, str], 车辆ID列索引或列名
         time_idx: Union[int, str], 帧号列索引或列名
@@ -49,7 +50,7 @@ class TimeSpacePlotter(ResearchPlt):
         '''
         super().__init__(**kwargs)
         self.path = path
-        self.output_dir = output_dir
+        self.save_dir = save_dir
         self.max_time = max_time
         self.v_trans = v_trans
         self.cmap = cmap
@@ -61,9 +62,15 @@ class TimeSpacePlotter(ResearchPlt):
         self.time_idx = time_idx if isinstance(time_idx, str) else self.data.columns[time_idx]
         self.dist_idx = dist_idx if isinstance(dist_idx, str) else self.data.columns[dist_idx]
         self.v_idx = v_idx if isinstance(v_idx, str) else self.data.columns[v_idx]
-        self.data = self.data.sort_values(by=[self.lane_idx, self.time_idx, self.car_idx],
+        self.data = self.data.sort_values(by=[self.time_idx, self.car_idx, self.lane_idx],
                                           axis=0, ascending=[True, True, True])
+        if ids is not None:
+            if not isinstance(ids, list):
+                ids = [ids]
+            self.data = self.data[self.data[self.car_idx].isin(ids)]
         self.data = self.data.reset_index(drop=True)
+        # 暂时保存处理结果
+        self.data.to_csv(os.path.join(save_dir, 'tmp_data.csv'), index=False)
 
     def run(self,
             x_min: int = None, x_max: int = None, x_gap: int = None, x_offset: int = 0,
@@ -95,6 +102,7 @@ class TimeSpacePlotter(ResearchPlt):
         data[self.v_idx] = data[self.v_idx] * 3.6 if self.v_trans else data[self.v_idx]
         data[self.time_idx] = data[self.time_idx] + x_offset
         data[self.dist_idx] = data[self.dist_idx] + y_offset
+        norm_func = plt.Normalize(colorbar_min, colorbar_max)
         # 画图
         print("begin drawing!")
         handle = tqdm(data.groupby(data[self.lane_idx]))
@@ -103,8 +111,10 @@ class TimeSpacePlotter(ResearchPlt):
             plt.figure()
             for _, car_traj in lane_data.groupby(lane_data[self.car_idx]):
                 car_traj[self.v_idx] = car_traj[self.v_idx].abs()  # 取abs，以免v方向与指定方向相反而为负
+                speeds = car_traj[self.v_idx].values
                 plt.scatter(car_traj[self.time_idx], car_traj[self.dist_idx],
-                            c=list(car_traj[self.v_idx]), cmap=self.cmap, s=self.markersize)
+                            c=speeds, cmap=self.cmap, norm=norm_func,
+                            s=self.markersize)
             plt.title(f"lane {lane} trajectories")
             self.colorbar_vehicle_speed(
                 cmap=self.cmap, v_min=colorbar_min, v_max=colorbar_max, v_step=colorbar_step)
@@ -120,7 +130,7 @@ class TimeSpacePlotter(ResearchPlt):
                 y_grid=y_grid, y_grid_color=y_grid_color,
                 y_grid_style=y_grid_style, y_grid_width=y_grid_width,
                 )
-            plt.savefig(os.path.join(self.output_dir, f"lane_{lane}.jpg"))
+            plt.savefig(os.path.join(self.save_dir, f"lane_{lane}.jpg"))
             plt.close()
         print("finish drawing!")
 
@@ -128,9 +138,9 @@ class TimeSpacePlotter(ResearchPlt):
 def main_example():
     '''样例数据画图'''
     path = 'data/tra_sample.xlsx'
-    output_dir = path.removesuffix('.xlsx')
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
+    save_dir = path.removesuffix('.xlsx')
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
     # 数据表列索引
     lane_idx = 1
     car_idx = 0
@@ -139,7 +149,7 @@ def main_example():
     v_idx = 6
     # 运行
     tsp = TimeSpacePlotter(
-        path=path, output_dir=output_dir,
+        path=path, save_dir=save_dir,
         lane_idx=lane_idx, car_idx=car_idx, time_idx=time_idx,
         dist_idx=dist_idx, v_idx=v_idx,
         v_trans=True, markersize=2,
@@ -152,9 +162,9 @@ def main_sumo_model0():
     '''sumo仿真model0数据画图'''
     path = r'D:\myscripts\pro\output\model0\trajectory.csv'
     # 创建输出文件夹
-    output_dir = os.path.join(os.path.dirname(path), 'trajectory')
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
+    save_dir = os.path.join(os.path.dirname(path), 'trajectory')
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
     # 数据表列索引
     lane_idx = 'laneID'
     car_idx = 'vehicleID'
@@ -164,9 +174,9 @@ def main_sumo_model0():
     v_idx = 'speed(m/s)'
     # 运行
     tsp = TimeSpacePlotter(
-        path=path, output_dir=output_dir,
+        path=path, save_dir=save_dir,
         lane_idx=lane_idx, car_idx=car_idx, time_idx=time_idx,
-        dist_idx=dist_idx, v_idx=v_idx,
+        dist_idx=dist_idx, v_idx=v_idx, ids=list(range(100)),       # 
         v_trans=True, markersize=1,
         figsize=(20,8),
         )
@@ -177,6 +187,36 @@ def main_sumo_model0():
         )
 
 
+def test_sumo_model0_single_car():
+    '''sumo仿真model0数据画图, 单辆车'''
+    path = r'D:\myscripts\pro\output\model0\trajectory.csv'
+    df = pd.read_csv(path)
+    for car_id in df['vehicleID'].unique():
+        print(car_id)
+        save_dir = os.path.join(os.path.dirname(path), 'trajectory', f'{car_id}')
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
+        tmp_file_path = os.path.join(save_dir, 'tmp.csv')
+        df[df['vehicleID'] == car_id].to_csv(tmp_file_path, index=False)
+        # 数据表列索引
+        lane_idx = 'laneID'
+        car_idx ='vehicleID'
+        time_idx = 'time(s)'
+        dist_idx= 'x(m)'            # 'distance(m)'
+        v_idx ='speed(m/s)'
+        # 运行
+        tsp = TimeSpacePlotter(
+            path=tmp_file_path, save_dir=save_dir,
+            lane_idx=lane_idx, car_idx=car_idx, time_idx=time_idx,
+            dist_idx=dist_idx, v_idx=v_idx, v_trans=True,
+        )
+        tsp.run(
+            x_min=0, x_max=2200,
+            y_min=0, y_max=2500, y_gap=50, y_offset=1000,
+            y_grid=[1000, 1500], 
+        )
+
 if __name__ == '__main__':
     # main_example()
     main_sumo_model0()
+    # test_sumo_model0_single_car()
